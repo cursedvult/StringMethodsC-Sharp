c8bed8a8d9664cbf181098f6c25a58b79c4a29ca
Gustavo Miguel De Barros Carranço <GutMig1112@outlook.com>
2023-03-09 05:36:52 -0300

    Just an update.

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -9,11 +9,19 @@ namespace Lesson3
             string car = "sandero";
             string sentence = "My words are not the bigger, but are the better";
             string nullify = "";
+            //Trims through all of the string
             Console.WriteLine(car.Trim());
+            //Finds the index where certain word or character is placed on the string
             Console.WriteLine(sentence.IndexOf("bigger"));
+            //Verifies if the string is null or just has white spaces.
+            //If yes, returns true, if not, returns false
             bool nult = String.IsNullOrWhiteSpace(nullify);
             Console.WriteLine(nult);
-            //Console.WriteLine(car.Substring(5));
+            //Takes away characters from a string until it reaches a specified index
+            Console.WriteLine(car.Substring(5));
+            //String format puts an object and puts it in another 
+            //already existing string
+            Console.WriteLine(String.Format("My words are better. {0}", sentence));
         }
 
     }

6aac083c329ebf35c4dcaea49ab7decb70cf078b
HOME <HOME@DESKTOP-1EQJ8DU>
2023-03-09 05:27:34 -0300

    Add project files.

---
+++ Lesson3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string car = "sandero";
+            string sentence = "My words are not the bigger, but are the better";
+            string nullify = "";
+            Console.WriteLine(car.Trim());
+            Console.WriteLine(sentence.IndexOf("bigger"));
+            bool nult = String.IsNullOrWhiteSpace(nullify);
+            Console.WriteLine(nult);
+            //Console.WriteLine(car.Substring(5));
+        }
+
+    }
+}
